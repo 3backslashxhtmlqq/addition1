91264347231ea376bc099dab459eca17b20c16f3
3backslashxhtmlqq <officialmasrel@marsel.com>
2025-02-07 21:15:31 +0300

    дополнительное задание

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите n: ");
+        int n = scanner.nextInt();
+
+        int temp = 0;
+
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+                System.out.println(n);
+            } else {
+                n = 3 * n + 1;
+                System.out.println(n);
+            }
+            temp++;
+        }
+        System.out.println("Кол-во шагов: " + temp);
+    }
+}
